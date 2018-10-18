df29829d5b91d60ac7fc11a503c271f57bc75267
Susan Meyers <smmeyers24@gmail.com>
2018-10-18 03:14:14 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,7 +5,7 @@ class Person
                 :wrist_size, :glove_size, :pant_length, :pant_width
   
   def initialize(:name:, :birthday:, :hair_color:, :eye_color:, :height:, 
-    # :weight:, :handed:, :complexion:, :t_shirt_size:, 
-    # :wrist_size:, :glove_size:, :pant_length:, :pant_width:)
+     :weight:, :handed:, :complexion:, :t_shirt_size:, 
+     :wrist_size:, :glove_size:, :pant_length:, :pant_width:)
   end
 end
